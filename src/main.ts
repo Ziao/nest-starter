f93bfd7377c392d0654774fe8aea5c54f4ed2211
Nick <nick@ziaomedia.com>
2023-09-16 02:23:04 +0200

    feat: cors (*), port from env

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const logger = new Logger('App');
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 5000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         cors: false,
     });
 
+    app.enableCors({
+        origin: '*',
+    });
     // app.useLogger(app.get(Logger));
     app.useGlobalPipes(
         new ValidationPipe({
@@ -41,7 +44,7 @@ const configureSwagger = (app: INestApplication) => {
         .setDescription('The todo API description')
         .setVersion('1.0')
         .addServer('http://localhost:3000') // todo: env
-        .addTag('default')
+        // .addTag('default')
         .build();
     // https://docs.nestjs.com/openapi/introduction#document-options
     const document = SwaggerModule.createDocument(app, config);
